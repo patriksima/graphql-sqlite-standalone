eeb06fe390eab6017aa0d814f47e7dce01eaf1e1
Patrik Šíma <172640+patriksima@users.noreply.github.com>
2020-04-01 21:21:25 +0200

    init

---
+++ App.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using GraphQL.SystemTextJson;
+using GraphQL.Types;
+
+namespace GraphQLTest
+{
+    public class App : IDisposable
+    {
+        private static readonly Lazy<App>
+            Lazy =
+                new Lazy<App>
+                    (() => new App());
+
+        private MyServer _server;
+
+        private App()
+        {
+        }
+
+        public static App Instance => Lazy.Value;
+
+        public void Dispose()
+        {
+            _server.OnAccept -= OnAcceptUtf8;
+        }
+
+        public void Run()
+        {
+            _server = new MyServer(3);
+            _server.Start(9000);
+            _server.OnAccept += OnAcceptUtf8;
+            _server.Listen();
+        }
+
+        private void OnAcceptUtf8(HttpListenerContext context)
+        {
+            var request = context.Request;
+            if (!request.HasEntityBody)
+            {
+                return;
+            }
+
+            using var body = request.InputStream;
+            using var reader = new StreamReader(body, request.ContentEncoding);
+            var content = reader.ReadToEnd();
+
+            Debug.WriteLine($"Content: {content}");
+
+            var utf8JsonReader = new Utf8JsonReader(Encoding.ASCII.GetBytes(content));
+            var js = JsonSerializer.Deserialize<GQuery>(ref utf8JsonReader,
+                new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+
+            Debug.WriteLine($"Query: {js.Query}");
+            OnQuery(context, js.Query);
+        }
+
+        private async void OnQuery(HttpListenerContext context, string query)
+        {
+            var schema = Schema.For(@"
+          type Droid {
+            id: String!
+            name: String!
+            friend: Character
+          }
+
+          type Character {
+            name: String!
+          }
+
+          type Query {
+            hero: Droid
+          }
+        ", _ =>
+            {
+                _.Types.Include<DroidType>();
+                _.Types.Include<Query>();
+            });
+
+
+            var json = await schema.ExecuteAsync(_ => { _.Query = query; });
+
+
+            var response = context.Response;
+            // Construct a response.
+            var responseString = json;
+            var buffer = Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            var output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+        }
+    }
+}
---
+++ MyServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+
+namespace GraphQLTest
+{
+    public class MyServer : IDisposable
+    {
+        private readonly HttpListener _listener = new HttpListener();
+
+        public Action<HttpListenerContext> OnAccept;
+
+        public MyServer(int maxThreads)
+        {
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void Start(int port)
+        {
+            _listener.Prefixes.Add($@"http://+:{port}/graphql/");
+            _listener.Start();
+        }
+
+        public void Listen()
+        {
+            while (_listener.IsListening)
+            {
+                var result = _listener.BeginGetContext(ar =>
+                {
+                    var listener = ar.AsyncState as HttpListener;
+                    var context = listener?.EndGetContext(ar);
+                    OnAccept?.Invoke(context);
+                }, _listener);
+                result.AsyncWaitHandle.WaitOne();
+            }
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+}
